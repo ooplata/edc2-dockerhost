4512efcca0dd5060d23b93e290e242bd8b039df4
amyepes <99094213+amyepes@users.noreply.github.com>
2025-02-07 14:50:38 -0500

    Create run_benchmark.sh

---
+++ run_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /benchmark/benchmark-scripts
+
+echo "Lenguaje | Tiempo (ms)" > results.txt
+
+for dir in Go JavaScript; do
+    echo "Ejecutando $dir..."
+    cd $dir
+    docker build -t ${dir}_benchmark .
+    tiempo=$(docker run --rm ${dir}_benchmark)
+    echo "$dir | $tiempo" >> ../results.txt
+    cd ..
+done
+
+cat results.txt
